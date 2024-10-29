c7cb5e9a90cff2f4f48de1bd78e698aa2dc1972c
Lucas Caponi <prateadow@gmail.com>
2024-10-29 01:34:55 -0300

    refactor: tests

--- tests/unit/fuzzy_search_spec.js
+++ tests/unit/fuzzy_search_spec.js
@@ -17,7 +17,7 @@ describe('FuzzyPokemonSearch', () => {
   });
 
   describe('Configuração de Opções de Busca', () => {
-    test('deve configurar opções de busca corretamente', () => {
+    test('configura opções de busca corretamente', () => {
       const opcoes = mecanismoDeBusca.configurarOpcoesDeBusca();
       expect(opcoes).toEqual({
         keys: ['name', 'habitat'],
@@ -26,7 +26,7 @@ describe('FuzzyPokemonSearch', () => {
       });
     });
 
-    test('deve permitir alteração de threshold nas opções de busca', () => {
+    test('permite alteração de threshold nas opções de busca', () => {
       mecanismoDeBusca.precisaoDaBusca = 0.4;
       const opcoes = mecanismoDeBusca.configurarOpcoesDeBusca();
       expect(opcoes.threshold).toBe(0.4);
@@ -34,7 +34,7 @@ describe('FuzzyPokemonSearch', () => {
   });
 
   describe('Busca Fuzzy com Erros de Digitação', () => {
-    test('deve retornar resultado para erro de digitação em "name" (ex: "picachu" -> "pikachu")', async () => {
+    test('retorna resultado para erro de digitação em "name" (ex: "picachu" -> "pikachu")', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'picachu' },
         usarClausulaANDParaBusca: false
@@ -45,26 +45,24 @@ describe('FuzzyPokemonSearch', () => {
       ]);
     });
 
-    test('deve conter o objeto pikachu na resposta', async () => {
+    test('verifica presença do objeto pikachu no habitat "forest"', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { habitat: 'forest' },
         usarClausulaANDParaBusca: false
       });
-    
-      console.log(resultado);
-    
+
       expect(resultado.data).toEqual(
         expect.arrayContaining([
           expect.objectContaining({
             name: 'pikachu',
             habitat: 'forest',
-            type: expect.any(Array) // Verifica que 'type' é um array sem se preocupar com os detalhes
+            type: expect.any(Array)
           })
         ])
       );
-    });    
+    });
 
-    test('deve retornar resultado para erro de digitação em "type" (ex: "electrik" -> "electric")', async () => {
+    test('retorna resultado para erro de digitação em "type" (ex: "electrik" -> "electric")', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { type: 'electrik' },
         usarClausulaANDParaBusca: false
@@ -74,7 +72,7 @@ describe('FuzzyPokemonSearch', () => {
       ]);
     });
 
-    test('deve retornar resultado vazio para erro de digitação muito distante', async () => {
+    test('retorna resultado vazio para erro de digitação muito distante', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'xyz' },
         usarClausulaANDParaBusca: false
@@ -84,7 +82,7 @@ describe('FuzzyPokemonSearch', () => {
   });
 
   describe('Combinações de Fuzzy Search', () => {
-    test('1 - apenas nome', async () => {
+    test('apenas nome', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'pikachu' },
         usarClausulaANDParaBusca: false
@@ -92,7 +90,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'pikachu' })]);
     });
 
-    test('2 - apenas tipo', async () => {
+    test('apenas tipo', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { type: 'fire' },
         usarClausulaANDParaBusca: false
@@ -100,7 +98,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'charmander', type: ['fire'] })]);
     });
 
-    test('3 - apenas habitat', async () => {
+    test('apenas habitat', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { habitat: 'cave' },
         usarClausulaANDParaBusca: false
@@ -108,7 +106,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'zubat', habitat: 'cave' })]);
     });
 
-    test('4 - nome e tipo', async () => {
+    test('nome e tipo', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'charmander', type: 'fire' },
         usarClausulaANDParaBusca: true
@@ -116,7 +114,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'charmander', type: ['fire'] })]);
     });
 
-    test('5 - nome e habitat', async () => {
+    test('nome e habitat', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'bulbasaur', habitat: 'grassland' },
         usarClausulaANDParaBusca: true
@@ -124,7 +122,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'bulbasaur', habitat: 'grassland' })]);
     });
 
-    test('6 - habitat e tipo', async () => {
+    test('habitat e tipo', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { habitat: 'forest', type: 'electric' },
         usarClausulaANDParaBusca: true
@@ -132,7 +130,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'pikachu', habitat: 'forest', type: ['electric'] })]);
     });
 
-    test('7 - nome, habitat e tipo', async () => {
+    test('nome, habitat e tipo', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'venusaur', type: 'grass', habitat: 'grassland' },
         usarClausulaANDParaBusca: true
@@ -149,7 +147,7 @@ describe('FuzzyPokemonSearch', () => {
       );
     });
 
-    test('8 - ordem diferente dos critérios', async () => {
+    test('ordem diferente dos critérios', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { habitat: 'grassland', type: 'grass', name: 'bulbasaur' },
         usarClausulaANDParaBusca: true
@@ -157,7 +155,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'bulbasaur', habitat: 'grassland', type: ['grass'] })]);
     });
 
-    test('9 - tipo como primeira posição do array de tipos', async () => {
+    test('tipo como primeira posição do array de tipos', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { type: 'bug' },
         usarClausulaANDParaBusca: false
@@ -165,7 +163,7 @@ describe('FuzzyPokemonSearch', () => {
       expect(resultado.data).toEqual([expect.objectContaining({ name: 'scyther', type: ['bug', 'flying'] })]);
     });
 
-    test('10 - tipo como segunda posição do array de tipos', async () => {
+    test('tipo como segunda posição do array de tipos', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { type: 'flying' },
         usarClausulaANDParaBusca: false
@@ -177,55 +175,13 @@ describe('FuzzyPokemonSearch', () => {
     });
   });
 
-  describe('Busca com Lógica AND e OR', () => {
-    test('deve retornar resultados de busca fuzzy com lógica OR para múltiplos critérios', async () => {
-      const resultado = await mecanismoDeBusca.buscar({
-        criterioDeBusca: { name: 'picachu', habitat: 'forest' },
-        usarClausulaANDParaBusca: false
-      });
-      expect(resultado.data).toEqual(expect.arrayContaining([
-        expect.objectContaining({ name: 'pikachu' })
-      ]));
-    });
-
-    test('deve retornar resultados de busca fuzzy com lógica AND para múltiplos critérios', async () => {
-      const resultado = await mecanismoDeBusca.buscar({
-        criterioDeBusca: { name: 'zubat', habitat: 'cave' },
-        usarClausulaANDParaBusca: true
-      });
-      expect(resultado.data).toEqual([
-        expect.objectContaining({ name: 'zubat', habitat: 'cave' })
-      ]);
-    });
-
-    test('deve retornar vazio quando nenhum critério coincide para lógica OR', async () => {
-      const resultado = await mecanismoDeBusca.buscar({
-        criterioDeBusca: { name: 'invalid', habitat: 'invalid' },
-        usarClausulaANDParaBusca: false
-      });
-      expect(resultado.data).toEqual([]);
-    });
-
-    test('deve retornar todos os resultados que atendem a qualquer critério com lógica OR', async () => {
-      const resultado = await mecanismoDeBusca.buscar({
-        criterioDeBusca: { name: 'zubat', type: 'fire' },
-        usarClausulaANDParaBusca: false
-      });
-      expect(resultado.data).toEqual(expect.arrayContaining([
-        expect.objectContaining({ name: 'zubat' }),
-        expect.objectContaining({ name: 'charmander' })
-      ]));
-    });
-  });
-
   describe('Paginação de Resultados', () => {
-    test('deve paginar corretamente os resultados fuzzy', async () => {
+    test('pagina corretamente os resultados fuzzy', async () => {
       const resultado = await mecanismoDeBusca.buscar({
-        criterioDeBusca: { name: 'a' }, // Critério genérico para garantir que retorne todos os itens
+        criterioDeBusca: { name: 'a' },
         usarClausulaANDParaBusca: false,
         pagina: 1
       });
-        
       expect(resultado.meta).toEqual({
         current_page: 1,
         total_pages: 3,
@@ -233,14 +189,13 @@ describe('FuzzyPokemonSearch', () => {
         items_in_current_page: 2
       });
     });
-  
-    test('deve retornar meta correta para página sem resultados', async () => {
+
+    test('retorna meta correta para página sem resultados', async () => {
       const resultado = await mecanismoDeBusca.buscar({
         criterioDeBusca: { name: 'zubat' },
         usarClausulaANDParaBusca: false,
         pagina: 10 
       });
-    
       expect(resultado.data).toEqual([]);
       expect(resultado.meta).toEqual({
         current_page: 10,
@@ -249,16 +204,15 @@ describe('FuzzyPokemonSearch', () => {
         items_in_current_page: 0
       });
     });
-  
-    test('deve ajustar total de páginas corretamente para diferentes tamanhos de página', async () => {
+
+    test('ajusta total de páginas corretamente para diferentes tamanhos de página', async () => {
       const buscaComItensPorPagina3 = new FuzzyPokemonSearch(dadosDePokemon, 3);
       const resultado = await buscaComItensPorPagina3.buscar({
-        criterioDeBusca: { name: 'a' }, // Critério genérico para garantir que retorne todos os itens
+        criterioDeBusca: { name: 'a' },
         usarClausulaANDParaBusca: false,
         pagina: 1
       });
-    
-      expect(resultado.meta.total_pages).toBe(2); 
+      expect(resultado.meta.total_pages).toBe(2);
     });
   });
 });
